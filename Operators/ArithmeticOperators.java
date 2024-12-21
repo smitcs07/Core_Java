fa62d8c858e9e5da1dfd2e9a2f25263523d13521
smit07 <smitchaurasiya9598@gmail.com>
2024-12-22 02:04:49 +0530

    new

---
+++ Operators/ArithmeticOperators.java
@@ -0,0 +1,20 @@
+public class ArithmeticOperators {
+  public static void main(String[] args) {
+      // Arithmetic operators on integers
+      int a = 10;
+      int b = 3;
+
+      // Arithmetic operators on Strings
+      String n1 = "15";
+      String n2 = "25";
+
+      //Integer.parseInt convert string into integer.
+      int a1 = Integer.parseInt(n1);
+      int a2 = Integer.parseInt(n2);
+      System.out.println("a + b = "+(a+b));
+      System.out.println("a - b = "+(a-b));
+      System.out.println("a / b = "+(a/b));
+      System.out.println("a * b = "+(a*b));
+      System.out.println("a % b = "+(a%b));
+  }
+}
---
+++ Operators/UnaryOperators.java
@@ -0,0 +1,14 @@
+public class UnaryOperators {
+  public static void main(String []args){
+    
+    int a = 12;
+    int b = 24;
+
+    System.out.println("a is Increament by 1: "+(a++));
+    System.out.println("b is Increament by 1: "+(++b));
+    System.out.println("a is decreament by 1: "+(a--));
+    System.out.println("b is decreament by 1: "+(--b));
+    System.out.println("The value of a is: "+(a));
+    System.out.println("The value of b is: "+(b));
+  }
+}
